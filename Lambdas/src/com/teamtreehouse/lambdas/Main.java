b4679540794077952e56ad15e26efbbd6842d650
justinm-nyc <justin.martinelli@oracle.com>
2019-11-12 13:53:17 -0500

    Added functions

--- Lambdas/src/com/teamtreehouse/lambdas/Main.java
+++ Lambdas/src/com/teamtreehouse/lambdas/Main.java
@@ -20,8 +20,28 @@ public class Main {
         }
     }
 
+    public static void usingLambdasInLongForm(){
+        List<Book> books = Books.all();
+        Collections.sort(books, (Book b1, Book b2) -> {
+            return b1.getTitle().compareTo(b2.getTitle());
+        });
+
+        for (Book book: books) {
+            System.out.println(book);
+        }
+    }
+
+    public static void usingLambdasInShortForm(){
+        List<Book> books = Books.all();
+        Collections.sort(books, Comparator.comparing(Book::getTitle));
+
+        for (Book book: books) {
+            System.out.println(book);
+        }
+    }
+
     public static void main(String[] args) {
 	// write your code here
-        usingAnonymousInlineClass();
+        usingLambdasInShortForm();
     }
 }
